eccca480169927e6a426402a19f9f10624ec6fa9
BorjaAM <borja.gallifrey@gmail.com>
2020-10-16 13:53:15 +0100

    Project and Tests created

---
+++ src/test/java/org/example/FizzBuzz_Test.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzz_Test {
+
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+    @Test
+    public void should_return_number_when_is_not_3_or_5_multiple(){
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+        assertThat(fizzBuzz.of(1)).isEqualTo("2");
+    }
+
+    @Test
+    public void should_return_Fizz_when_is_multiple_of_3(){
+        assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.of(6)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.of(9)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_return_Buzz_when_is_multiple_of_5(){
+        assertThat(fizzBuzz.of(5)).isEqualTo("Buzz");
+        assertThat(fizzBuzz.of(10)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_is_multiple_of_15(){
+        assertThat(fizzBuzz.of(15)).isEqualTo("FizzBuzz");
+        assertThat(fizzBuzz.of(30)).isEqualTo("FizzBuzz");
+    }
+    public static class FizzBuzz {
+
+        public String of(int i) {
+            return String.valueOf(i);
+        }
+    }
+}
